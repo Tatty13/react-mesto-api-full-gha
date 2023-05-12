82bc5c729fe36c512e9ba980bbff4bc22ea1f641
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-12 18:32:46 +0300

    feat: add crash test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,6 +9,14 @@ const { validateUserCredential } = require('../middlewares/validators/user-valid
 
 const { login, createUser, logout } = require('../controllers/users');
 
+/* ----------- crash-test -------------*/
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+/*-------------------------------------*/
+
 router.post('/signin', validateUserCredential, login);
 router.post('/signup', validateUserCredential, createUser);
 router.post('/signout', auth, logout);
