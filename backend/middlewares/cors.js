53a714c7423a02d5d986caa42e0f7870797a0d79
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-12 12:12:13 +0300

    fix: allow credentials in handleCors middleware; use res.set instead of res.header

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -13,15 +13,21 @@ function handleCors(req, res, next) {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.set({
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Credentials': true,
+    });
   }
 
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.set({
+      'Access-Control-Allow-Methods': DEFAULT_ALLOWED_METHODS,
+      'Access-Control-Allow-Headers': requestHeaders,
+    });
     res.end();
     return;
   }
+
   next();
 }
 
