589875e91e376ac1bd0d10504580bb424a83f0c9
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-12 12:55:06 +0300

    refactor: change baseURL options, remove authorization header from apiOptions; style: correct format with eslint and prettier

--- frontend/src/utils/apiOptions.js
+++ frontend/src/utils/apiOptions.js
@@ -1,26 +1,25 @@
 const apiOptions = {
-  baseURL: `https://mesto.nomoreparties.co/v1/cohort-59`,
+  baseURL: `https://api.mesto.russia.nomoredomains.monster`,
   headers: {
-    authorization: "ed75e1f6-f274-4143-80f6-0a17df0f2155",
-    "Content-Type": "application/json; charset=UTF-8",
+    'Content-Type': 'application/json; charset=UTF-8',
   },
   errorMessages: {
-    getUserData: "Не удалось загрузить данные пользователя",
-    setUserData: "Не удалось обновить данные пользователя",
-    getInitialCards: "Не удалось загрузить карточки",
-    addCard: "Не удалось создать карточку",
-    deleteCard: "Не удалось удалить карточку",
-    toogleCardLike: "Невозможно выполнить действие",
-    updateAvatar: "Не удалось обновить аватар",
+    getUserData: 'Не удалось загрузить данные пользователя',
+    setUserData: 'Не удалось обновить данные пользователя',
+    getInitialCards: 'Не удалось загрузить карточки',
+    addCard: 'Не удалось создать карточку',
+    deleteCard: 'Не удалось удалить карточку',
+    toogleCardLike: 'Невозможно выполнить действие',
+    updateAvatar: 'Не удалось обновить аватар',
   },
 };
 
 const authApiOptions = {
-  baseURL: "https://auth.nomoreparties.co",
+  baseURL: 'https://api.mesto.russia.nomoredomains.monster',
   errorMessages: {
-    authUser: "Не удалось зарегистрировать пользователя",
-    login: "Не удалось авторизовать пользователя",
-    validateToken: "Данные не прошли проверку",
+    authUser: 'Не удалось зарегистрировать пользователя',
+    login: 'Не удалось авторизовать пользователя',
+    validateToken: 'Данные не прошли проверку',
   },
 };
 
